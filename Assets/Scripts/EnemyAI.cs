4f111ca5c56a3789b7bfe97eddc498139d2785f9
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2024-02-20 13:37:40 +1100

    20/02/24
    Enemy takes damage from sword.
    Sword damage on a switch-case pulling values from HealthManager bloodCurrent.
    Still need to implement damage to player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,26 +7,37 @@ public class EnemyAI : MonoBehaviour
     [Header("Object Reference")]
     private GameObject player;
     public GameObject bloodDrop;
+    private Rigidbody2D rigidBody;
 
     [Header("Movement Variables")]
     [SerializeField] private float speed = 2f;
 
     [Header("Health & Damage Variables")]
-    [SerializeField] private int enemyHealthMax;
+    [SerializeField] private int enemyHealthMax = 10;
     public int enemyHealthCurrent;
     [SerializeField] private int enemyDamage;
+
+    [Header("Stun Timers")]
+    [SerializeField] private float stunCounter;
+    [SerializeField] private float stunTotalTime;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("PlayerCharacter");
         enemyHealthCurrent = enemyHealthMax;
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        stunCounter -= Time.deltaTime;
+
+        if (stunCounter <= 0)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
 
         if (enemyHealthCurrent <= 0)
         {
@@ -35,8 +46,11 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    void TakeDamage(int damageTaken)
+    public void TakeDamage(int damageTaken, bool knockFromRight)
     {
+        stunTotalTime = (damageTaken / 10f);
+        stunCounter = stunTotalTime;
+
         enemyHealthCurrent = enemyHealthCurrent - damageTaken;
     }
 }
